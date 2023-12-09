50ecd2e2877fb284d372b9abd68c9e2739c98959
Kirill Lazarenko <lazarenkokiri10@gmail.com>
2023-12-09 10:37:17 +0200

    second step

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,2 +1,47 @@
-package org.example;public class StringCalculator {
+package org.example;
+
+public class StringCalculator {
+
+    public int add(String numbers) {
+        // Check if the input string is empty
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        // Split the input string by commas
+        String[] numArray = numbers.split(",");
+
+        // Initialize sum to 0
+        int sum = 0;
+
+        // Iterate through the numbers and add them to the sum
+        for (String num : numArray) {
+            // Check for consecutive commas or empty values
+            if (num.isEmpty()) {
+                throw new IllegalArgumentException("Input contains consecutive commas or empty values");
+            }
+
+            // Convert each number to an integer
+            int n = Integer.parseInt(num);
+            // Add the number to the sum
+            sum += n;
+        }
+
+        // Return the sum
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        StringCalculator calculator = new StringCalculator();
+
+        try {
+            System.out.println(calculator.add(""));      // Output: 0
+            System.out.println(calculator.add("1"));     // Output: 1
+            System.out.println(calculator.add("1,2"));   // Output: 3
+            System.out.println(calculator.add("1,2,3")); // Output: 6
+        } catch (IllegalArgumentException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,4 +1,48 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StringCalculatorTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void testEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void testSingleString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+    }
+
+    @Test
+    public void testTwoNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    public void testMoreThanTwoNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(6, calculator.add("1,2,3"));
+    }
+
+    @Test
+    public void testFourNumbers() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(10, calculator.add("1,2,3,4"));
+    }
 }
